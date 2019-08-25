3b6c3694fc9baf637bebd1344c430cfe866e56b9
CEM ONUR BAS <cemonurbas@CEM-MacBook-Pro.local>
2019-08-25 23:48:26 +0300

    Extract header bar

--- src/App.js
+++ src/App.js
@@ -1,54 +1,17 @@
 import React, {Component} from 'react';
+import Header from "./components/Header";
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  trends = [{"title": "Home", "link": "home.html"}, {"title": "Literature", "link": "literature.html"}]
+  
   render() {
     return (
       <div className="site-wrap">
-
-        <div className="site-mobile-menu">
-          <div className="site-mobile-menu-header">
-            <div className="site-mobile-menu-close mt-3">
-              <span className="icon-close2 js-menu-toggle"></span>
-            </div>
-          </div>
-          <div className="site-mobile-menu-body"></div>
-        </div>
         
-        <header className="site-navbar" role="banner">
-          <div className="container-fluid">
-            <div className="row align-items-center">
-              
-              <div className="col-12 search-form-wrap js-search-form">
-                <form method="get" action="#">
-                  <input type="text" id="s" className="form-control" placeholder="Search..."/>
-                  <button className="search-btn" type="submit"><span className="icon-search"></span></button>
-                </form>
-              </div>
-
-              <div className="col-4 site-logo">
-                <a href="index.html" className="text-black h2 mb-0">Mini Blog</a>
-              </div>
-
-              <div className="col-8 text-right">
-                <nav className="site-navigation" role="navigation">
-                  <ul className="site-menu js-clone-nav mr-auto d-none d-lg-block mb-0">
-                    <li><a href="category.html">Home</a></li>
-                    <li><a href="category.html">Politics</a></li>
-                    <li><a href="category.html">Tech</a></li>
-                    <li><a href="category.html">Entertainment</a></li>
-                    <li><a href="category.html">Travel</a></li>
-                    <li><a href="category.html">Sports</a></li>
-                    <li className="d-none d-lg-inline-block"><a href="#" className="js-search-toggle"><span className="icon-search"></span></a></li>
-                  </ul>
-                </nav>
-                <a href="#" className="site-menu-toggle js-menu-toggle text-black d-inline-block d-lg-none"><span className="icon-menu h3"></span></a></div>
-              </div>
-
-          </div>
-        </header>
-        
+        <Header trends={this.trends} />
         
         <div className="site-section bg-light">
           <div className="container">
---
+++ src/components/Header.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Trend = ({trends}) => (
+    <ul className="site-menu js-clone-nav mr-auto d-none d-lg-block mb-0">
+        {
+            trends.map((trend, i) =>  {
+                return (
+                    <li><a href={trend.link}>{trend.title}</a></li>
+                )
+            })
+        }
+        <li className="d-none d-lg-inline-block"><a href="#" className="js-search-toggle"><span className="icon-search"></span></a></li>
+    </ul>
+)
+
+class Header extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+	render() {
+		const { trends } = this.props
+		return (
+            <div>
+                <div className="site-mobile-menu">
+                    <div className="site-mobile-menu-header">
+                    <div className="site-mobile-menu-close mt-3">
+                        <span className="icon-close2 js-menu-toggle"></span>
+                    </div>
+                    </div>
+                    <div className="site-mobile-menu-body"></div>
+                </div>
+                
+                <header className="site-navbar" role="banner">
+                    <div className="container-fluid">
+                    <div className="row align-items-center">
+                        
+                        <div className="col-12 search-form-wrap js-search-form">
+                        <form method="get" action="#">
+                            <input type="text" id="s" className="form-control" placeholder="Search..."/>
+                            <button className="search-btn" type="submit"><span className="icon-search"></span></button>
+                        </form>
+                        </div>
+
+                        <div className="col-4 site-logo">
+                        <a href="index.html" className="text-black h2 mb-0">Mini Blog</a>
+                        </div>
+
+                        <div className="col-8 text-right">
+                        <nav className="site-navigation" role="navigation">
+                            <Trend trends={trends} />
+                        </nav>
+                        <a href="#" className="site-menu-toggle js-menu-toggle text-black d-inline-block d-lg-none"><span className="icon-menu h3"></span></a></div>
+                        </div>
+
+                    </div>
+                </header>
+            </div>
+        )
+    }
+}
+
+Header.propTypes = {
+    trends: PropTypes.array
+}
+
+export default Header
