104b56e76f07ade3d2344baad6d99d67815b7023
Cem O. Bas <cemonurbas@yahoo.com>
2020-06-01 00:14:44 +0300

    Update services uri

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,4 @@
-const env = process.env;
-
-export const nodeEnv = env.NODE_ENV || 'development';
+export const nodeEnv = process.env.NODE_ENV || 'development';
 
 export const logStars = function(message) {
   console.info('**********');
@@ -9,7 +7,7 @@ export const logStars = function(message) {
 };
 
 export default {
-  uri: env.E77SRV_URI || `http://localhost:8080`,
+  uri: process.env.E77SRV_URI || `http://localhost:8080`,
   get serverUrl() {
     return `${this.uri}`;
   }
