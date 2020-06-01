e948e9a8c63d49735d9b2d196eb46566d699f09d
Cem O. Bas <cemonurbas@yahoo.com>
2020-06-01 14:46:10 +0300

    Switch media endpoint to public jpg links

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,14 +1,14 @@
 const media = process.env.REACT_APP_E77MEDIA_URI || 'http://localhost:8081';
 
 export function getArticleImgUrlById(articleId, imgIndex, unwrapped) {
-    console.log(`${media}/posts/${articleId}/${imgIndex}`);
+    console.log(`${media}/posts/${articleId}_${imgIndex}.jpg`); // remove jpg, if endpoint is e77-media
     if(unwrapped) {
-        return `${media}/posts/${articleId}/${imgIndex}`;
+        return `${media}/posts/${articleId}_${imgIndex}.jpg`;
     }
-    return `url('${media}/posts/${articleId}/${imgIndex}')`;
+    return `url('${media}/posts/${articleId}_${imgIndex}.jpg')`;
 }
 
 export function getAuthorImgUrlById(nickname) {
-    console.log(`${media}/authors/${nickname}`);
-    return `${media}/authors/${nickname}`;
+    console.log(`${media}/authors/${nickname}.jpg`);
+    return `${media}/authors/${nickname}.jpg`;
 }
